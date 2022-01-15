2aa2f7a3ed0a90a5e1a12b4f616aae6687b59069
tae-1230 <74037846+tae-1230@users.noreply.github.com>
2022-01-14 20:07:44 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(30,30,20,20)
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x + 4;
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x - 4;
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.position.y = box.position.y - 4;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y = box.position.y + 4;
+  }
+  
+
+}
+
+
+
+
